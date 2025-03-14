a0142a42a5dd8e0c09b1782257a2be4b14170df9
KavyaTJ <kavyatj2609@gmail.com>
2025-03-14 22:01:59 +0530

    Updated styles for timer

--- src/challenges/timer/Timer.tsx
+++ src/challenges/timer/Timer.tsx
@@ -1,43 +1,52 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
+import styles from "./Timer.module.css";
 
 function Timer() {
-    const [minutes, setMinutes] = useState(5);
-    const [seconds, setSeconds] = useState(0);
-    const [isActive, setIsActive] = useState(false);
+  const [minutes, setMinutes] = useState(5);
+  const [seconds, setSeconds] = useState(0);
+  const [isActive, setIsActive] = useState(false);
 
-     useEffect(() => {
-        if(isActive){
+  useEffect(() => {
+    if (isActive) {
         const interval = setInterval(() => {
-            setSeconds(prevSeconds => {
+            setSeconds((prevSeconds) => {
                 if (prevSeconds === 0) {
-                    return setMinutes(prevMinutes => {
-                        if (prevMinutes === 0) {
-                            clearInterval(interval);
-                            return 0;
-                        }
-                        return prevMinutes - 1;
-                    }), 59;
+                    return 59;
                 }
                 return prevSeconds - 1;
             });
-            
-        }, 1000);
-        return () => {
-            clearInterval(interval);
-        }
-    }
-    }
-    ,[isActive,minutes]);
 
-    
-    const handleTimer = () => setIsActive(!isActive);
+            setMinutes((prevMinutes) => {
+                if (minutes === 0 && seconds === 0) {
+                    clearInterval(interval);
+                    return 0;
+                }
+                if (seconds === 0) {
+                    return prevMinutes - 1;
+                }
+                return prevMinutes;
+            });
+        }, 1000);
+
+        return () => clearInterval(interval);
+    }
+}, [isActive, seconds]); 
+
+  const handleTimer = () => setIsActive(!isActive);
+
   return (
-    <div>
-        <h2>Timer</h2>
-        <h3>{minutes}:{seconds}</h3>
-        <button onClick={handleTimer}>{isActive? 'stop':'start'}</button>
+    <div className={styles.timerContainer}>
+      <h2 className={styles.timerTitle}>Beautiful Timer</h2>
+      <div className={styles.timerCircle}>
+        <h3 className={styles.timerDisplay}>
+          {minutes}:{seconds < 10 ? `0${seconds}` : seconds}
+        </h3>
+      </div>
+      <button onClick={handleTimer} className={styles.timerButton}>
+        {isActive ? "Stop" : "Start"}
+      </button>
     </div>
-  )
+  );
 }
 
-export default Timer
+export default Timer;
